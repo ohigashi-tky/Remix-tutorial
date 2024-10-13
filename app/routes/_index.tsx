e02e6334d0ca7d29dc2bc72ee1f97b3d8d57a545
ohigashi-tky <97085725+ohigashi-tky@users.noreply.github.com>
2024-10-13 13:58:42 +0900

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+    return (
+      <p id="index-page">
+        これは Remix のデモです。
+        <br />
+        <a href="https://remix.run">remix.run のドキュメント</a> を確認してください。
+      </p>
+    );
+  }
